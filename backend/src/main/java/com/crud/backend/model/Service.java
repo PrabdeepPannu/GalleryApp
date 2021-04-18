01178dd4ab4852301f3470ac8eabc40793015f77
PrabdeepPannu <pann0008@algonquinlive.com>
2021-04-17 23:54:41 -0400

    Spring initialing and Model setups

---
+++ backend/src/main/java/com/crud/backend/model/Service.java
@@ -0,0 +1,52 @@
+package com.crud.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "service", indexes = @Index(name = "idx_service_name", columnList = "name"))
+public class Service {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(name = "id", nullable = false, updatable = false,columnDefinition = "VARCHAR(36)")
+    private UUID id;
+
+    @Column(name = "name")
+    private String Name;
+
+    @Column(name = "type")
+    private String Type;
+
+    @Column(name = "icon_url")
+    private String iconUrl;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "service", cascade = CascadeType.ALL)
+    private List<Model> models = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "service", cascade = CascadeType.ALL)
+    private List<Metric> metrics = new ArrayList<>();
+
+}
