a64e0a2b1c36a35e6ad5c3f3352e6a653f9f4af7
PrabdeepPannu <pann0008@algonquinlive.com>
2021-04-18 00:03:57 -0400

    spring controllers

---
+++ backend/src/main/java/com/crud/backend/controller/ModelController.java
@@ -0,0 +1,61 @@
+package com.crud.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import com.crud.backend.repository.ModelRepository;
+import com.crud.backend.model.Model;
+import com.crud.backend.model.Metric;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ModelController {
+    @Autowired
+    private ModelRepository modelRepository;
+
+    @GetMapping("/model")
+    public List<Model> GetModels() {
+        return modelRepository.findAll();
+    }
+
+    @GetMapping("/model/{id}")
+    public Model GetModel(@PathVariable Integer id) {
+        return modelRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/model/{id}/metric")
+    public List<Metric> GetMetrics(@PathVariable Integer id) {
+        Model model = modelRepository.findById(id).orElse(null);
+        if (model != null) {
+            return model.getMetrics();
+        }
+        return null;
+    }
+
+    @PostMapping("/model")
+    public Model PostModel(@RequestBody Model model) {
+        return modelRepository.save(model);
+    }
+
+    @PutMapping("/model")
+    public void PutModel(@RequestBody Model model) {
+        Model oldModel = modelRepository.findById(model.getId()).orElse(null);
+        oldModel.setApi(oldModel.getApi());
+        oldModel.setName(oldModel.getName());
+        oldModel.setUserName(oldModel.getUserName());
+        oldModel.setPassword(oldModel.getPassword());
+        oldModel.setQuery(oldModel.getQuery());
+        modelRepository.save(oldModel);
+    }
+
+}
