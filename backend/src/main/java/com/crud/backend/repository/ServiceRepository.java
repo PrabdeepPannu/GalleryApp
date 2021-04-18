392573b39c9afa1cea755ee97d49a7e248bb4865
PrabdeepPannu <pann0008@algonquinlive.com>
2021-04-17 23:59:12 -0400

    spring repositries

---
+++ backend/src/main/java/com/crud/backend/repository/ServiceRepository.java
@@ -0,0 +1,10 @@
+package com.crud.backend.repository;
+
+import com.crud.backend.model.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+
+}
