c17b19c163c6e42cf3b4d4d8a66f981ae6a5860b
PrabdeepPannu <pann0008@algonquinlive.com>
2021-04-21 20:18:39 -0400

    fix

--- frontend/src/options.global.js
+++ frontend/src/options.global.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    api: 'http://localhost:8080/api',
-    api_metric: 'http://localhost:8080/api/metric',
-    api_model: 'http://localhost:8080/api/model',
-    api_service: 'http://localhost:8080/api/service',
+    api: 'https://gallery-app-server.herukoapp.com/api',
+    api_metric: 'https://gallery-app-server.herukoapp.com/api/metric',
+    api_model: 'https://gallery-app-server.herukoapp.com/model',
+    api_service: 'https://gallery-app-server.herukoapp.com/service',
 };
