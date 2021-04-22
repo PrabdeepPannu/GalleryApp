a641d3efba0f92b3c53b79fe5e63e7f307398b85
PrabdeepPannu <pann0008@algonquinlive.com>
2021-04-21 20:32:53 -0400

    fix

--- frontend/src/options.global.js
+++ frontend/src/options.global.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    api: 'https://gallery-app-server.herokuoapp.com/api',
+    api: 'https://gallery-app-server.herokuapp.com/api',
     api_metric: 'https://gallery-app-server.herokuapp.com/api/metric',
     api_model: 'https://gallery-app-server.herokuapp.com/model',
     api_service: 'https://gallery-app-server.herokuapp.com/service',
